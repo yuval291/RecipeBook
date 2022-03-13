49233f3070b728c04df9be0c03d6d6d529781f48
yuval291 <yuval291195@gmail.com>
2022-03-13 14:32:44 +0200

    saving current state

--- src/app/shared/loading-spinner/loading-spinner.component.ts
+++ src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-loading-spinner',
     template: '<div class="lds-ring"><div></div><div></div><div></div><div></div></div>',
-    styleUrls: ['./loading-spinner.component.ts']
+    styleUrls: ['./loading-spinner.component.css']
 })
 export class LoadingSpinnerComponent {
     
